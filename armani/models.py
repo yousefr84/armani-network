b0a0c76dfb86659fc67df9dee49b0efc722dd814
yousefr84 <m.yousef.r84@gmail.com>
2024-11-26 09:55:47 +0330

    create Mentors model

--- armani/models.py
+++ armani/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
-class mentors(models.Model):
+class Services(models.Model):
+    services = {
+        "MT": 'مشاوره فروش و مارکتینگ',
+        "IM": 'سرمایه گذاری',
+        "FB": 'تدوین طرح توجیهی و طرح کسب و کار',
+        "FA": 'خدمات مالی و حسابداری',
+        "LG": 'خدمات حقوقی',
+    }
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=2, choices=services)
+
+class Mentors(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    family_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.IntegerField()
+    Job_position = models.CharField(max_length=100)
+    Services = models.ManyToManyField(Services)
+
+
